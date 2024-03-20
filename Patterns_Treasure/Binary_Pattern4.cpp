1c085dcd3f374a9d92fd2b0889ddaad50241c830
Mohit Kumawat <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2024-03-20 18:49:21 +0530

    Update Binary_Pattern4.cpp

--- Patterns_Treasure/Binary_Pattern4.cpp
+++ Patterns_Treasure/Binary_Pattern4.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main()
 {
-    /* Easiest Logic Explained Again
-       When sum of i and j is divisible by 2 then print 0 else print 1
+    /*Easiest Logic Explained Again
+     When sum of i and j is divisible by 2 then print 0 else print 1
    Same logic but this time starting is from 1 so we will start the J loop from 0 so that divisibility is set to 1 onwards
     At first point i=1 and j=0 then 1+0%2 = 1 print 1
     Secondly i=2 and j=0 then 2+0%2 = 0 so print 0 and so on ........
