ff792a073f409f230e6fef3af7fa5146bf6f81c7
Mohit Kumawat <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2024-03-20 18:57:00 +0530

    Absolute Functions using Star Patterns

---
+++ Patterns_Treasure/Pattern9.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+#include <math.h>
+
+int main()
+{
+    //Using Absolute Value Function
+    int n = 4;
+
+    for(int i = n; i >= -n; i--)
+    {
+        for(int j = n; j >= abs(i); j--)
+        {
+            cout << "#";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+/*
+
+Output : 
+
+#
+##
+###
+####
+#####
+####
+###
+##
+#
+
+*/
