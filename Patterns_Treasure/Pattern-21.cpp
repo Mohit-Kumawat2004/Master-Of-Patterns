4851faad2247e55b1fb163127405071e1701e734
Mohit Kumawat <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2024-03-22 21:55:42 +0530

    Mystery Patterns

---
+++ Patterns_Treasure/Pattern-21.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main() {
+    int n = 5;
+ 
+    for (int i = 1; i <= n; i++) 
+    {
+       for (int j = 1; j <= n; j++)
+       {
+            cout <<abs(i-j) << " ";
+       }
+            cout << endl;
+    }
+  
+  return 0;
+}
+
+
+/*
+
+
+Output:
+
+0 1 2 3 4 
+1 0 1 2 3 
+2 1 0 1 2 
+3 2 1 0 1 
+4 3 2 1 0 
+
+
+
+*/
