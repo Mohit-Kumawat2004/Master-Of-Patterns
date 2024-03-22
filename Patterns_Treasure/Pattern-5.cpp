3b38aaf406705636698ca42ccce15a3d635958c2
Mohit Kumawat <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2024-03-22 07:33:11 +0430

    Update Pattern-5.cpp

---
+++ Patterns_Treasure/Pattern-5.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //Opposite Right Angle Pattern 
+    int n;
+    cout<<"Enter Number : ";
+    cin>>n;
+    int i, j;
+    for(i=1; i<=n; i++)
+    {
+        //For Space 
+        for(j=1; j<=n-i; j++){
+            cout<<" ";
+        }
+        //Star print from last 
+        for(j=1; j<=i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+/*
+
+Output : 
+
+Enter Number : 8
+       *
+      **
+     ***
+    ****
+   *****
+  ******
+ *******
+********
+
+*/
