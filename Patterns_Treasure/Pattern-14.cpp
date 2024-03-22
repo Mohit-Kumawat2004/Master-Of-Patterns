31536798e674474658846e5da5337742787df095
Mohit Kumawat <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2024-03-22 21:34:22 +0530

    New Style Pattern 2

---
+++ Patterns_Treasure/Pattern-14.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 9; // size
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (j == 1 || i == j || i == n) {
+                std::cout << "*";
+            } else {
+                std::cout << " "; // 2ws
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+
+/*
+
+
+Output:
+
+*        
+**       
+* *      
+*  *     
+*   *    
+*    *   
+*     *  
+*      * 
+*********
+
+
+
+*/
