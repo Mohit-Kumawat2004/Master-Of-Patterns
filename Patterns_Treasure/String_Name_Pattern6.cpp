b09b3efe685aa365a77d6f64ab5b350524b60339
Mohit Kumawat <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2024-03-23 09:49:22 +0530

    Inverted Triangle Pattern String Name

---
+++ Patterns_Treasure/String_Name_Pattern6.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+
+    char arr[] = "MOHIT";
+    int len = strlen(arr);
+
+    for(int i = 0; i < len; i++)
+    {
+
+        for(int j = i; j >= 0; j--)
+        {
+            cout<<" "; // White Spaces 
+        }
+
+        for(int j = 0; j < len - i; j++)
+        {
+            cout<<arr[j]<<" ";
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+
+
+/*
+
+Output : 
+
+
+ M O H I T 
+  M O H I 
+   M O H 
+    M O 
+     M 
+
+
+*/
