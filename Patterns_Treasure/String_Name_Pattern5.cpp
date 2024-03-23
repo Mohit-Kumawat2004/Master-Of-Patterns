b05e478859a541fd60cbcd3993b07d9503482907
Mohit Kumawat <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2024-03-23 09:44:03 +0530

    Triangle Name Pattern

---
+++ Patterns_Treasure/String_Name_Pattern5.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+    char arr[] = "MOHIT";
+    int len = strlen(arr);
+
+    for(int i = 0; i < len; i++)
+    {
+
+        for(int j = len-1; j >= i; j--)
+        {
+            cout<<" "; // White Spaces
+        }
+
+        for(int j = 0; j <= i; j++)
+        {
+            cout<<arr[j]<<" ";
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+
+/*
+
+
+Output : 
+
+     M 
+    M O 
+   M O H 
+  M O H I 
+ M O H I T 
+
+
+
+*/
