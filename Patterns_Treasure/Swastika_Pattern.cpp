cb5b3432c0bba8abb3b030b46ba1db52dfcb7531
Mohit Kumawat <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2024-03-23 16:09:11 +0530

    Update Patterns_Treasure
    working - First if else - addition pattern, Second if else - upper left side, Third if else - upper right side , Fourth if else - lower left side , Last if else - lower right side 
    
    Else - print spaces

---
+++ Patterns_Treasure/Swastika_Pattern.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /*  Best Suits For Odd Numbers and is a constant Type of Pattern  */
+    
+    int n = 13;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i == n / 2 + 1 || j == n / 2 + 1 ||
+                (i == 1 && j >= n / 2 + 1) ||
+                (i == n && j <= n / 2) ||
+                (j == 1 && i<=n/2+1) ||
+                (i>=n / 2 + 1 && j == n)) {
+                cout << "* ";
+            } else {
+                cout << "  "; // White spaces
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+
+/*
+
+
+Output : 
+
+
+*           * * * * * * * 
+*           *             
+*           *             
+*           *             
+*           *             
+*           *             
+* * * * * * * * * * * * * 
+            *           * 
+            *           * 
+            *           * 
+            *           * 
+            *           * 
+* * * * * * *           *
+
+
+
+*/
