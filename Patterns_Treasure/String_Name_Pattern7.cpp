840abf3424b4a41d570394cb24ef436a02bb59ae
Mohit Kumawat <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2024-03-23 09:54:57 +0530

    Addition Style Patten

---
+++ Patterns_Treasure/String_Name_Pattern7.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    char str[]="MOHIT";
+    int len=strlen(str);
+    int i,j;
+
+    for (i=0; i<len; i++)
+    {
+
+        for (j=0; j<len; j++)
+        {
+           
+            if (i==len/2)
+                cout<<str[j]<<" ";
+
+            else if (j==len/2)
+                cout<<str[i]<<" ";
+
+            else
+                cout<<" "; //2ws
+
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+
+
+/*
+
+Output : 
+
+    M   
+    O   
+M O H I T 
+    I   
+    T   
+
+
+*/
