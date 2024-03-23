ee537b133081128509fcb42a224126797b7c4901
Mohit Kumawat <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2024-03-23 12:28:36 +0530

    Cross Pattern in Name

---
+++ Patterns_Treasure/String_Name_Pattern8.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+int main()
+{ 
+    // Name Pattern in Form of Cross
+    char arr[]="MOHIT";
+    int len = strlen(arr);
+    int i,j;
+    
+    for(i=0; i<len; i++){
+        for(j=0; j<=len; j++){
+          
+            if(i==j || i+j == len-1){
+                cout<<arr[j];
+            }
+            else{
+                cout<<" ";
+            }
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+
+
+/*
+
+Output : 
+
+M       T 
+  O   I  
+    H    
+  O   I  
+M       T 
+
+
+
+*/
