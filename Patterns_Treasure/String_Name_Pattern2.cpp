4650b821de54d6b3f7bd6eb71d19be1d91b40436
Mohit Kumawat <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2024-03-23 07:01:02 +0530

    Name Patterns 2
    Interesting Name Patterns

---
+++ Patterns_Treasure/String_Name_Pattern2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    //The char array to hold the string and print a single char at a time.
+    //Reverse String Order in Rectangle Pattern 
+    
+    
+    char arr[] = "MOHIT";
+
+    int len = strlen(arr);
+    int i, j;
+
+    for (i = 0; i < len; i++)
+    {
+        for (j = 1; j <= len; j++)
+        {
+            cout << arr[len - j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+
+/*
+
+
+Output : 
+
+
+T I H O M 
+T I H O M 
+T I H O M 
+T I H O M 
+T I H O M 
+
+
+
+*/
