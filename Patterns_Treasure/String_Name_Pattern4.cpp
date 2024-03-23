0ceadee2406088f8c5db8ef8f4dfbcd39fdfb64d
Mohit Kumawat <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2024-03-23 07:11:02 +0530

    Done Style 4

---
+++ Patterns_Treasure/String_Name_Pattern4.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+    //The char array to hold the string and print a single char at a time.
+    //Inverted String Name Array
+    
+    
+    char arr[] = "MOHIT KUMAWAT";
+
+    int len = strlen(arr);
+    int i, j;
+
+    for (i = len - 1 ; i >= 0; i--)
+    {
+        for (j = 0; j <= i; j++)
+        {
+            cout << arr[j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+
+
+/*
+
+Output : 
+
+
+M O H I T   K U M A W A T 
+M O H I T   K U M A W A 
+M O H I T   K U M A W 
+M O H I T   K U M A 
+M O H I T   K U M 
+M O H I T   K U 
+M O H I T   K 
+M O H I T   
+M O H I T 
+M O H I 
+M O H 
+M O 
+M 
+
+
+
+*/
