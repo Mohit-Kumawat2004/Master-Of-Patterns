432892b10069232b93d144f0b354fdf48e930c69
Mohit Kumawat <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2024-03-23 07:07:04 +0530

    Done 3 style

---
+++ Patterns_Treasure/String_Name_Pattern3.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+    //The char array to hold the string and print a single char at a time.
+    
+    char arr[] = "MOHIT KUMAWAT";
+
+    int len = strlen(arr);
+    int i, j;
+
+    for (i = 0; i < len; i++)
+    {
+        for (j = 0; j <= i; j++)
+        {
+            cout << arr[j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+/*
+
+Output : 
+
+M 
+M O 
+M O H 
+M O H I 
+M O H I T 
+M O H I T   
+M O H I T   K 
+M O H I T   K U 
+M O H I T   K U M 
+M O H I T   K U M A 
+M O H I T   K U M A W 
+M O H I T   K U M A W A 
+M O H I T   K U M A W A T 
+
+
+
+
+*/
+
