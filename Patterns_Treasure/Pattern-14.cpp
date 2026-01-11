b07bc51541d6cc71451cddc268d8c174e3942c9c
Mohit Kumawat <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2026-01-11 09:10:18 +0530

    asdfghj

--- Patterns_Treasure/Pattern-14.cpp
+++ Patterns_Treasure/Pattern-14.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int n = 9;
+    int n = 9; 
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
             if (j == 1 || i == j || i == n) {
